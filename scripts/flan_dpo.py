0ce83b7310f1ba22fa10cfd69f5cf4862ebc0562
Robin Jaccard <robin.jaccard8@gmail.com>
2024-05-16 17:54:16 +0200

    simplify flan_dpo

--- scripts/flan_dpo.py
+++ scripts/flan_dpo.py
@@ -22,122 +22,49 @@ def load_config(config_file):
         config = yaml.safe_load(stream)
     return config
 
-def preprocess_function(examples, tokenizer, prefix="Please answer the question: "):
-    """Add a prefix to the inputs and tokenize the inputs and targets.
-
-    Args:
-        examples: dataset examples.
-        tokenizer: tokenizer.
-        prefix (str, optional): Prefix to add to each inputs. Defaults to "Please answer the question: ".
-
-    Returns:
-        dataset: processed examples.
-    """
-    # The "inputs" are the tokenized answer:
-    inputs = [prefix + doc for doc in examples["prompt"]]
-    model_inputs = tokenizer(inputs, max_length=512, truncation=True)
-
-    # The "labels" are the tokenized outputs:
-    chosen = tokenizer(
-        text_target=examples["chosen"], max_length=512, truncation=True
-    )
-    rejected = tokenizer(
-        text_target=examples["rejected"], max_length=512, truncation=True
-    )
-
-
-    model_inputs["chosen"] = chosen["input_ids"]
-    model_inputs["rejected"] = rejected["input_ids"]
-
-    return model_inputs
-
-def prepare_dataset(config, tokenizer, dev_mode=False):
-    """Load and preprocess the dataset.
-
-    Args:
-        cfg (dict): configuration.
-        tokenizer: tokenizer.
-        dev_mode (bool, optional): development mode. Defaults to False.
-
-    Returns:
-        Dataset: tokenized dataset.
-    """
-    dataset = load_dataset("json", data_files=config["train_dataset"])
-    if dev_mode:
-        dataset["train"] = dataset["train"].select(range(20))  # For development
-    else:
-        dataset["train"] = dataset["train"]#.select(range(427))  # For fine-tuning
-    dataset = dataset["train"].train_test_split(test_size=0.2)
-    #tokenized_dataset = dataset.map(
-        #preprocess_function,
-        #batched=True,
-        #fn_kwargs={"tokenizer": tokenizer},
-    #)
-    return dataset
-
 
 def train_model_fft_with_fft(config, device):
     # Load tokenizer and model
 
-    tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
-    model = AutoModelForSeq2SeqLM.from_pretrained(config['model_name'])#.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(config['model_path'])
+    model = AutoModelForSeq2SeqLM.from_pretrained(config['model_path'])
     print(print_number_of_trainable_model_parameters(model))
 
+    dataset_train = load_dataset("json", data_files=config["train_dataset"])
+    dataset_test = load_dataset("json", data_files=config["test_dataset"])
 
-    #low_cpu_mem_usage=True,
-    #torch_dtype=torch.float16
-
-
-    dataset = prepare_dataset(config, tokenizer, config['dev_mode'])
-    print(dataset)
     # Training arguments
     training_args = TrainingArguments(
-        per_device_train_batch_size=config['batch_size'],
-        gradient_accumulation_steps=config['batch_size'],
         num_train_epochs=config['num_epochs'],
-
         evaluation_strategy="steps",
-        eval_steps=1000,
+        eval_steps=config['eval_steps'],
         save_strategy="epoch",
-        
         output_dir=config['output_dir'],
         overwrite_output_dir=True,
         logging_steps=config['logging_steps'],
-        logging_dir=config['logging_dir'],
-        save_steps=10000,
         report_to="wandb",
         auto_find_batch_size=True,
-        #gradient_checkpointing=True,
-        learning_rate=2e-5,
-        #lr_scheduler_type="cosine",
-        #max_steps=max_steps,
-        #warmup_steps=max_steps//4,
+        learning_rate=config['lr'],
+        lr_scheduler_type=config['lr_scheduler'],
         )
 
     # DPO Trainer
     max_prompt_length = 512
     max_length = 1024
-    #data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 
-    print(dataset['train'])
-
-    print(dataset['test'])
     dpo_trainer = DPOTrainer(
         model=model,
         args=training_args,
-        train_dataset=dataset['train'],
-        eval_dataset=dataset['test'],
+        train_dataset=dataset_train['train'],
+        eval_dataset=dataset_test['train'],
         tokenizer=tokenizer,
-        #data_collator=data_collator,
-        beta=10,
-        max_target_length=512,
-        max_prompt_length=max_prompt_length,
+        beta=config['beta'],
+        max_target_length=max_prompt_length,
+        max_prompt_length=max_length,
         max_length=max_length)
 
     dpo_trainer.train()
     dpo_trainer.save_model(config['output_dir'] + "/full_model")
-
-
 
 
 def train_model_lora_with_lora(config, device):
@@ -148,8 +75,8 @@ def train_model_fft_with_lora(config, device):
 
     # Load tokenizer and model
     print('Loooora')
-    tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
-    model = AutoModelForSeq2SeqLM.from_pretrained(config['model_name'])#.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(config['model_path'])
+    model = AutoModelForSeq2SeqLM.from_pretrained(config['model_path'])
 
     lora_config = LoraConfig(
         r=64,  # Rank
@@ -162,67 +89,47 @@ def train_model_fft_with_lora(config, device):
     peft_model = get_peft_model(model, lora_config)
     print(print_number_of_trainable_model_parameters(peft_model))
 
-    #low_cpu_mem_usage=True,
-    #torch_dtype=torch.float16
 
-
-    dataset = prepare_dataset(config, tokenizer, config['dev_mode'])
+    dataset_train = load_dataset("json", data_files=config["train_dataset"])
+    dataset_test = load_dataset("json", data_files=config["test_dataset"])
 
     # Training arguments
     training_args = TrainingArguments(
-        per_device_train_batch_size=config['batch_size'],
-        gradient_accumulation_steps=config['batch_size'],
         num_train_epochs=config['num_epochs'],
         evaluation_strategy="steps",
-        eval_steps=100,
-        save_strategy="steps",
-        save_steps=100,
-        load_best_model_at_end=True,
+        eval_steps=config['eval_steps'],
+        save_strategy="epoch",
         output_dir=config['output_dir'],
         overwrite_output_dir=True,
         logging_steps=config['logging_steps'],
-        logging_dir=config['logging_dir'],
-        #save_steps=config['save_steps'],
         report_to="wandb",
         auto_find_batch_size=True,
-        #gradient_checkpointing=True,
-        learning_rate=2e-5,
-        #lr_scheduler_type="cosine",
-        #max_steps=max_steps,
-        #warmup_steps=max_steps//4,
+        learning_rate=config['lr'],
+        lr_scheduler_type=config['lr_scheduler'],
         )
 
     # DPO Trainer
     max_prompt_length = 512
     max_length = 1024
-    #data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 
-    print(dataset['train'])
 
-    print(dataset['test'])
     dpo_trainer = DPOTrainer(
         model=peft_model,
         args=training_args,
-        train_dataset=dataset['train'],
-        eval_dataset=dataset['test'],
+        train_dataset=dataset_train['train'],
+        eval_dataset=dataset_test['test'],
         tokenizer=tokenizer,
-        #data_collator=data_collator,
-        beta=10,
-        max_target_length=512,
+        beta=config['beta'],
+        max_target_length=max_length,
         max_prompt_length=max_prompt_length,
         max_length=max_length)
 
     dpo_trainer.train()
     dpo_trainer.save_model(config['output_dir'] + "/peft_model")
 
-
-
 if __name__ == "__main__":
     config_file = "configs/dpo_config.yaml"
     config = load_config(config_file)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Device:", device)
 
     os.environ["WANDB_PROJECT"] = "DPO"
     os.environ["WANDB_LOG_MODEL"] = "false"
